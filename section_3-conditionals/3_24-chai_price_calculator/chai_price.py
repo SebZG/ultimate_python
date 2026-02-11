e60ba0c5b3970049b9f419ce57198567c84b4975
Sebastian Zapata Gutierrez <sebastian.zapata.g@gmail.com>
2026-02-11 12:11:34 +0000

    Add: section 3

---
+++ section_3-conditionals/3_24-chai_price_calculator/chai_price.py
@@ -0,0 +1,10 @@
+cup = input("Choose your cup size (small/medium/large:) ").lower()
+
+if cup == "small":
+    print("Price is 10 rupees")
+elif cup == "medium":
+    print("Price is 20 rupees")
+elif cup == "large":
+    print("Price is 30 rupees")
+else:
+    print("Uknown cup size")
